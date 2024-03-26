2e1173a7b6f12a17ee1a7551063c631510350634
Bryan DeGrendel <bryan@degrendel.com>
2024-03-26 18:31:43 -0400

    Configure internal Maven repository

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -9,7 +9,65 @@ plugins {
 
 repositories {
     mavenCentral()
-    mavenLocal()
+}
+
+if (project.hasProperty("internalMavenURL")) {
+    val internalMavenUsername: String by project
+    val internalMavenPassword: String by project
+    val internalMavenURL: String by project
+
+    publishing {
+        publications {
+            create<MavenPublication>("maven") {
+                from(components["java"])
+            }
+        }
+        repositories {
+            maven {
+                credentials {
+                    username = internalMavenUsername
+                    password = internalMavenPassword
+                }
+                val releasesRepoUrl = "$internalMavenURL/releases/"
+                val snapshotsRepoUrl = "$internalMavenURL/snapshots/"
+                url = uri(if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl)
+                name = "Internal-Maven-Publish"
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            credentials {
+                username = internalMavenUsername
+                password = internalMavenPassword
+            }
+            url = uri("$internalMavenURL/releases")
+            name = "Internal-Maven-Releases"
+        }
+    }
+
+    repositories {
+        maven {
+            credentials {
+                username = internalMavenUsername
+                password = internalMavenPassword
+            }
+            url = uri("$internalMavenURL/snapshots")
+            name = "Internal-Maven-Snapshots"
+        }
+    }
+} else {
+    repositories {
+        mavenLocal()
+    }
+    publishing {
+        publications {
+            create<MavenPublication>("maven") {
+                from(components["java"])
+            }
+        }
+    }
 }
 
 dependencies {
@@ -18,14 +76,6 @@ dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testImplementation("org.junit.jupiter:junit-jupiter-engine:5.9.3")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-}
-
-publishing {
-    publications {
-        create<MavenPublication>("maven") {
-            from(components["java"])
-        }
-    }
 }
 
 java {
